79069e199e154f53250ec68d91a031f93506dfa4
Dereck Padden <dereckp@neurable.com>
2019-02-12 17:13:25 -0500

    basic outline of SGD implementd, need to apply Riemannian logic

--- eigentorch.py
+++ eigentorch.py
@@ -10,6 +10,7 @@ https://doi.org/10.1109/CVPR.2014.132
 """
 import torch
 nn = torch.nn
+from torch.optim.optimizer import Optimizer, required
 
 
 class BiMap(torch.autograd.Function):
@@ -29,12 +30,55 @@ class BiMap(torch.autograd.Function):
         Computes input gradients for BiMap Bilinear transformation.  The gradient for the transformations weights
         represent the Riemannian gradient with respect to the tangent of the Stiefel manifold
         NOTE: Results are only valid if inputs to forward are symmetric matrices!
+
+        Args:
+            params (iterable): iterable of parameters to optimize or dicts defining
+                parameter groups
+            lr (float): learning rate
         """
         X, W = ctx.saved_tensors
         grad_X = torch.mm(torch.mm(W.t(), grad_output), W)
         grad_W = 2 * grad_output.mm(W.mm(X))
         grad_W_r = grad_W - grad_W.mm(W.t().mm(W))
         return grad_X, grad_W_r
+
+
+class StiefelOpt(Optimizer):
+    """
+    Implements Parameter optimization with respect to a gradient on the Steifel manifold.
+    Expects that the gradient associated with a given Parameter is the Riemannian gradient
+    tangent to the manifold (i.e. gradient generated from BiMap function)
+    """
+    def __init__(self, params, lr=required):
+        if lr is not required and lr < 0.0:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        defaults = dict(lr=lr)
+        super(StiefelOpt, self).__init__(params, defaults)
+
+    def __setstate__(self, state):
+        super(StiefelOpt, self).__setstate__(state)
+        # for group in self.param_groups:
+        #     group.setdefault('placeholder', False)
+
+    def step(self, closure=None):
+        """Performs a single optimization step.
+
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                d_p = p.grad.data
+                p.data.add_(-group['lr'], d_p)
+
+        return loss
 
 
 if __name__ == "__main__":
